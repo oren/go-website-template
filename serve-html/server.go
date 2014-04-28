37a3c5836b0a9af6c6694b98520a388e79b1f87c
Oren <ogolan@yp.com>
2014-04-27 17:00:01 -0700

    serve html

---
+++ serve-html/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+        "io/ioutil"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+        dat, err := ioutil.ReadFile("index.html")
+        check(err)
+        fmt.Fprintf(w, string(dat))
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}

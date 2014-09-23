aaa50adad3e5acc43756f587ad8cf51e5205916b
Oren <ogolan@yp.com>
2014-09-22 18:34:26 -0700

    post

--- post/post
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-curl -X POST localhost:8080 -d "{\"A\":1, \"B\":4}"
--- post/server.go
+++ post/server.go
@@ -4,24 +4,50 @@ import (
         "log"
 	"net/http"
         "encoding/json"
+        "fmt"
 )
 
 func main() {
         http.Handle("/", http.FileServer(http.Dir("static")))
 	http.HandleFunc("/projects", newProject)
+	http.HandleFunc("/projects2", newProject2)
 	http.ListenAndServe(":8080", nil)
 }
 
+// post 2 integers
 func newProject(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, r.Method+" not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	req := struct{ A, B string }{}
+	req := struct{ A, B int }{}
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
+	res := struct{ Result int }{req.A}
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		log.Println(err)
+		http.Error(w, "oops", http.StatusInternalServerError)
+	}
+}
+
+// post 2 strings
+func newProject2(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, r.Method+" not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+        fmt.Printf("%+v\n", r.Body)
+
+	req := struct{ A, B string }{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+                fmt.Println("wow. error")
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+        fmt.Println("life is good")
 	res := struct{ Result string }{req.A}
 	if err := json.NewEncoder(w).Encode(res); err != nil {
 		log.Println(err)

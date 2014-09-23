0aeb19dd4bf30a64b87367b22538cdcd3918b15a
Oren <ogolan@yp.com>
2014-09-22 17:46:09 -0700

    strings

--- post/server.go
+++ post/server.go
@@ -17,12 +17,12 @@ func newProject(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, r.Method+" not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	req := struct{ A, B int }{}
+	req := struct{ A, B string }{}
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
-	res := struct{ Result int }{req.A + req.B}
+	res := struct{ Result string }{req.A}
 	if err := json.NewEncoder(w).Encode(res); err != nil {
 		log.Println(err)
 		http.Error(w, "oops", http.StatusInternalServerError)
